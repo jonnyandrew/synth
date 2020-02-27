2a8c9279e641693509eed4a2998e6154b3dd344f
Jonathan Andrew <jonny.andrew@protonmail.com>
2020-02-27 09:53:29 +0000

    Add tests for changing envelope parameters

--- app/src/test/cpp/Envelope_test.cpp
+++ app/src/test/cpp/Envelope_test.cpp
@@ -6,258 +6,293 @@
 
 using namespace synth;
 
-TEST(EnvelopeTest, WhenItIsTheFirstEnvelopeItStartsAfterZero) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 1000> buffer;
+namespace {
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+    const int ONE_FRAME_PER_SEC = 1000;
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.01);
-}
+    TEST(EnvelopeTest, WhenItIsTheFirstEnvelopeItStartsAfterZero) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1000> buffer;
 
-TEST(EnvelopeTest, WhenAttackIsNotTriggeredItOutputsZeros) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 100> buffer;
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    envelope.getSignal(buffer.data(), buffer.size());
-
-    for (int i = 0; i < 100; i++) {
-        EXPECT_FLOAT_EQ(buffer[i], 0);
+        EXPECT_FLOAT_EQ(buffer[0], 0.01);
     }
-}
 
-TEST(EnvelopeTest, ItCompletesTheAttackAtTheCorrectFrame) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 1000> buffer;
+    TEST(EnvelopeTest, WhenAttackIsNotTriggeredItOutputsZeros) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 100> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_GT(buffer[99], buffer[98]);
-    EXPECT_GT(buffer[99], buffer[100]);
-}
+        for (int i = 0; i < 100; i++) {
+            EXPECT_FLOAT_EQ(buffer[i], 0);
+        }
+    }
 
-TEST(EnvelopeTest, TheAttackReachesMaximum) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 1000> buffer;
+    TEST(EnvelopeTest, ItCompletesTheAttackAtTheCorrectFrame) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1000> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[99], 1.0);
-}
+        EXPECT_GT(buffer[99], buffer[98]);
+        EXPECT_GT(buffer[99], buffer[100]);
+    }
 
-TEST(EnvelopeTest, ItReachesTheSustainLevelAtTheCorrectFrame) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 1000> buffer;
+    TEST(EnvelopeTest, TheAttackReachesMaximum) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1000> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[200], 0.5);
-    EXPECT_FLOAT_EQ(buffer[999], 0.5);
-}
+        EXPECT_FLOAT_EQ(buffer[99], 1.0);
+    }
 
-TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringAttack) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 25> buffer;
+    TEST(EnvelopeTest, ItReachesTheSustainLevelAtTheCorrectFrame) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1000> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.25);
+        EXPECT_FLOAT_EQ(buffer[200], 0.5);
+        EXPECT_FLOAT_EQ(buffer[999], 0.5);
+    }
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+    TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringAttack) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 25> buffer;
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.26);
-}
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringDecay) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 150> buffer;
+        EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.25);
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.75);
+        EXPECT_FLOAT_EQ(buffer[0], 0.26);
+    }
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+    TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringDecay) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 150> buffer;
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.76);
-}
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringSustain) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 1000> buffer;
+        EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.75);
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.5);
+        EXPECT_FLOAT_EQ(buffer[0], 0.76);
+    }
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+    TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedDuringSustain) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1000> buffer;
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.51);
-}
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
+
+        EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.5);
+
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
+
+        EXPECT_FLOAT_EQ(buffer[0], 0.51);
+    }
 
 
-TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedAfterRelease) {
-    EnvelopeParameters params = {100, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 100> buffer;
+    TEST(EnvelopeTest, ItContinuesFromWhereThePreviousEnvelopeStoppedAfterRelease) {
+        EnvelopeParameters params = {100, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 100> buffer;
 
-    envelope.startAttack();
-    envelope.startRelease();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.startRelease();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.0);
+        EXPECT_FLOAT_EQ(buffer[buffer.size() - 1], 0.0);
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.01);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0.01);
+    }
 
-TEST(EnvelopeTest, WhenAttackIsZeroItStartsDecayImmediately) {
-    EnvelopeParameters params = {0, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenAttackIsZeroItStartsDecayImmediately) {
+        EnvelopeParameters params = {0, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.995);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0.995);
+    }
 
-TEST(EnvelopeTest, WhenAttackIsNearZeroItStartsDecayImmediately) {
-    EnvelopeParameters params = {0.0001, 100, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenAttackIsNearZeroItStartsDecayImmediately) {
+        EnvelopeParameters params = {0.0001, 100, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.995);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0.995);
+    }
 
-TEST(EnvelopeTest, WhenDecayIsZeroItDecaysStraightToSustain) {
-    EnvelopeParameters params = {5, 0, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenDecayIsZeroItDecaysStraightToSustain) {
+        EnvelopeParameters params = {5, 0, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[4], 1);
-    EXPECT_FLOAT_EQ(buffer[5], 0.5);
-}
+        EXPECT_FLOAT_EQ(buffer[4], 1);
+        EXPECT_FLOAT_EQ(buffer[5], 0.5);
+    }
 
-TEST(EnvelopeTest, WhenDecayIsSmallItDecaysStraightToSustain) {
-    EnvelopeParameters params = {5, 0.5, 0.5, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenDecayIsSmallItDecaysStraightToSustain) {
+        EnvelopeParameters params = {5, 0.5, 0.5, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[5], 0.5);
-}
+        EXPECT_FLOAT_EQ(buffer[5], 0.5);
+    }
 
-TEST(EnvelopeTest, WhenDecayAndSustainAreZeroItDecaysStraightToZero) {
-    EnvelopeParameters params = {5, 0, 0.0, 100};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenDecayAndSustainAreZeroItDecaysStraightToZero) {
+        EnvelopeParameters params = {5, 0, 0.0, 100};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[4], 1);
-    EXPECT_FLOAT_EQ(buffer[5], 0);
-}
+        EXPECT_FLOAT_EQ(buffer[4], 1);
+        EXPECT_FLOAT_EQ(buffer[5], 0);
+    }
 
-TEST(EnvelopeTest, WhenAttackDecayAndReleaseAreZero) {
-    EnvelopeParameters params = {0, 0, 1.0, 0};
-    Envelope envelope(1000, params);
-    std::array<float, 1> buffer;
+    TEST(EnvelopeTest, WhenAttackDecayAndReleaseAreZero) {
+        EnvelopeParameters params = {0, 0, 1.0, 0};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 1);
+        EXPECT_FLOAT_EQ(buffer[0], 1);
 
-    envelope.startRelease();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startRelease();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0);
+    }
 
-TEST(EnvelopeTest, WhenAttackAndDecayAreZeroItDropsToSustainImmediately) {
-    EnvelopeParameters params = {0, 0, 0.1, 0};
-    Envelope envelope(1000, params);
-    std::array<float, 1> buffer;
+    TEST(EnvelopeTest, WhenAttackAndDecayAreZeroItDropsToSustainImmediately) {
+        EnvelopeParameters params = {0, 0, 0.1, 0};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 1> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.1);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0.1);
+    }
 
-TEST(EnvelopeTest, WhenReleaseIsStartedItDropsToZero) {
-    EnvelopeParameters params = {0, 0, 1.0, 10};
-    Envelope envelope(1000, params);
-    std::array<float, 10> buffer;
+    TEST(EnvelopeTest, WhenReleaseIsStartedItDropsToZero) {
+        EnvelopeParameters params = {0, 0, 1.0, 10};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 10> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), 1);
-    envelope.startRelease();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), 1);
+        envelope.startRelease();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_FLOAT_EQ(buffer[0], 0.9);
-    EXPECT_FLOAT_EQ(buffer[8], 0.1);
-    EXPECT_FLOAT_EQ(buffer[9], 0);
-}
+        EXPECT_FLOAT_EQ(buffer[0], 0.9);
+        EXPECT_FLOAT_EQ(buffer[8], 0.1);
+        EXPECT_FLOAT_EQ(buffer[9], 0);
+    }
 
-TEST(EnvelopeTest, WhenReleaseIsFinishedItBecomesExactlyZero) {
-    EnvelopeParameters params = {0, 0, 1.0, 10};
-    Envelope envelope(1000, params);
-    std::array<float, 100> buffer;
+    TEST(EnvelopeTest, WhenReleaseIsFinishedItBecomesExactlyZero) {
+        EnvelopeParameters params = {0, 0, 1.0, 10};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 100> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), 1);
-    envelope.startRelease();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), 1);
+        envelope.startRelease();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    EXPECT_EQ(buffer[11], 0);
-}
+        EXPECT_EQ(buffer[11], 0);
+    }
 
-TEST(EnvelopeTest, WhenReleaseIsStartedBeforeDecayIsFinishedThenItDecaysFirst) {
-    EnvelopeParameters params = {10, 10, 0.8, 4};
-    Envelope envelope(1000, params);
-    std::array<float, 30> buffer;
+    TEST(EnvelopeTest, WhenReleaseIsStartedBeforeDecayIsFinishedThenItDecaysFirst) {
+        EnvelopeParameters params = {10, 10, 0.8, 4};
+        Envelope envelope(ONE_FRAME_PER_SEC, params);
+        std::array<float, 30> buffer;
 
-    envelope.startAttack();
-    envelope.getSignal(buffer.data(), 10);
-    envelope.startRelease();
-    envelope.getSignal(buffer.data(), buffer.size());
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), 10);
+        envelope.startRelease();
+        envelope.getSignal(buffer.data(), buffer.size());
 
-    // Expect decay to 0.8
-    EXPECT_FLOAT_EQ(buffer[0], 0.98);
-    EXPECT_FLOAT_EQ(buffer[9], 0.8);
+        // Expect decay to 0.8
+        EXPECT_FLOAT_EQ(buffer[0], 0.98);
+        EXPECT_FLOAT_EQ(buffer[9], 0.8);
 
-    // Expect release to 0
-    EXPECT_FLOAT_EQ(buffer[10], 0.6);
-    EXPECT_FLOAT_EQ(buffer[12], 0.2);
-    EXPECT_FLOAT_EQ(buffer[13], 0.0);
+        // Expect release to 0
+        EXPECT_FLOAT_EQ(buffer[10], 0.6);
+        EXPECT_FLOAT_EQ(buffer[12], 0.2);
+        EXPECT_FLOAT_EQ(buffer[13], 0.0);
+    }
+
+    TEST(EnvelopeTest, WhenEnvelopeParametersAreChangedAfterInitItChangesTheOutput) {
+        EnvelopeParameters initialParams = {10, 10, 0.8, 4};
+        Envelope envelope(ONE_FRAME_PER_SEC, initialParams);
+        std::array<float, 2> buffer;
+
+        envelope.setEnvelopeParameters({1, 1, 0.5, 1});
+
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
+
+        EXPECT_FLOAT_EQ(buffer[0], 1.0);
+        EXPECT_FLOAT_EQ(buffer[1], 0.5);
+    }
+
+    TEST(EnvelopeTest, WhenEnvelopeParametersAreChangedDuringAttackItChangesTheOutput) {
+        EnvelopeParameters initialParams = {10, 10, 0.8, 4};
+        Envelope envelope(ONE_FRAME_PER_SEC, initialParams);
+        std::array<float, 2> buffer;
+
+        envelope.startAttack();
+        envelope.getSignal(buffer.data(), buffer.size());
+        envelope.setEnvelopeParameters({1, 1, 0.5, 1});
+        envelope.getSignal(buffer.data(), buffer.size());
+
+
+        // now at frame indices 2 and 3, the new envelope is already at sustain
+        EXPECT_FLOAT_EQ(buffer[0], 0.5);
+        EXPECT_FLOAT_EQ(buffer[1], 0.5);
+    }
 }
